3355a30b24d27705e0a9801c37f85ca0f740a8dd
Jaeger_705 <ojoyce01@gmail.com>
2023-11-06 16:00:23 -0500

    second commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#Question 1
+name = input("What is your name?")
+print("Hello, " + name)
+
+#Question 2
+odd_numbers = list(range(1, 101, 2))
+for odd_number in odd_numbers:
+    print(odd_number)
+
+
+
+#Question 3
+
+#Question 4
+
+#Question 5
